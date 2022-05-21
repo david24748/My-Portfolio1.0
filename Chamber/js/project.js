b3f3c3299c4e1e5d34e31a1306732fceb5841143
Juan Estrada <100156819+david24748@users.noreply.github.com>
2022-05-20 21:20:49 -0500

    update chamber

--- Chamber/js/project.js
+++ Chamber/js/project.js
@@ -15,7 +15,20 @@ datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
 const hambutton = document.querySelector('.primarynav');
 const mainnav = document.querySelector('.navigation')
 
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+hambutton.addEventListener("click", () => {mainnav.classList.toggle("responsive")}, false);
 
 // To solve the mid resizing issue with responsive class on
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove("responsive")};
+
+let d = new Date().getDate();
+
+const banner = document.getElementById("banner");
+if (d === 1 || d === 2) {
+	banner.style.display = "block";
+}
+
+const close = document.querySelector("#close");
+
+close.addEventListener("click", () => {
+	banner.style.display = "none";
+});
